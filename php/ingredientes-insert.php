14878d8cdf54756d0a5a3f0a62908bc002d97947
Stanislao <stanis.campregher@gmail.com>
2018-04-08 22:49:38 -0300

    WIP edição de ingrediente

--- php/ingredientes-insert.php
+++ php/ingredientes-insert.php
@@ -19,7 +19,7 @@
     $stmt->bindParam(':descricao', $descricao);
 
     if ($stmt->execute()){
-        header('Location: index.php');
+        header('Location: ingredientes.php');
     }
     else{
         echo "Erro ao cadastrar";
