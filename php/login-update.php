4171db225ea1e510af4ff1e88612db7fc2c7d70d
Bruno Araujo <brunooyeah@gmail.com>
2018-05-17 15:45:22 -0300

    Conclusão catálogo sem envio para o e-mail

--- php/login-update.php
+++ php/login-update.php
@@ -1,6 +1,16 @@
 <?php 
 require_once('init.php');
 
+    // pega o ID da URL
+    $id = isset($_GET['codUsuario']) ? (int) $_GET['codUsuario'] : null;
+ 
+    // valida o ID
+    if (empty($id))
+    {
+        echo "ID para alteração não definido";
+    exit;
+    }
+
         $PDO = db_connect();    
         $sql = 'UPDATE usuario SET nome_usuario=:nome, login_usuario=:login, email_usuario=:email';
 		$sql .= 'WHERE cod_usuario = :cod_usuario';
