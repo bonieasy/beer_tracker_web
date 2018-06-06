1b6ccd1d0d69ddd0e38dd7ac79ed35f39e13a46b
Bruno Araujo <brunooyeah@gmail.com>
2018-06-06 16:23:27 -0300

    Correções na tela index

--- php/index.php
+++ php/index.php
@@ -5,11 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-<<<<<<< Updated upstream
-    <link rel="shortcut icon" href="img/favicon.png">
-=======
-    <link rel="shortcut icon" href="/img/favicon.png">
->>>>>>> Stashed changes
 
     <title>GND | Systems</title>
 
